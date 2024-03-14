ad80b54ae7f851f321983538526feaf66d07b4b1
로렌 <118709290+fhkwon@users.noreply.github.com>
2024-03-14 20:05:45 +0900

    [Bronze IV] Title: 영수증, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,12 @@
+total = int(input())
+n = int(input())
+
+s = 0
+for _ in range(n):
+    a, b = map(int, input().split())
+    s += a*b
+    
+if s == total:
+    print('Yes')
+else:
+    print('No')
